6c56119f1e5735cedb6d664e3fa0c06b063a86f0
varunrau <varunrau@gmail.com>
2014-02-26 15:18:08 -0800

    added fragment/layout for location

--- ff-android/ff-android/src/main/java/com/blueprint/ffandroid/LocationFragment.java
+++ ff-android/ff-android/src/main/java/com/blueprint/ffandroid/LocationFragment.java
@@ -77,12 +77,6 @@ public class LocationFragment extends Fragment {
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
