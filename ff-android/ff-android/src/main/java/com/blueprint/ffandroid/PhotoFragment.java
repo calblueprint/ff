9546b83574a2975a203e229f954ec929009c98eb
varunrau <varunrau@gmail.com>
2014-03-01 20:18:22 -0800

    added photo layout

---
+++ ff-android/ff-android/src/main/java/com/blueprint/ffandroid/PhotoFragment.java
@@ -0,0 +1,127 @@
+package com.blueprint.ffandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link PhotoFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link PhotoFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class PhotoFragment extends Fragment implements View.OnClickListener {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView mImageView;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PhotoFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PhotoFragment newInstance() {
+        PhotoFragment fragment = new PhotoFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+    public PhotoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView =  inflater.inflate(R.layout.fragment_photo, container, false);
+        mImageView = (ImageView) rootView.findViewById(R.id.photo_imageview);
+        setupClickListeners(rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+    public void setupClickListeners(View rootView) {
+        Button forward = (Button) rootView.findViewById(R.id.forward_button);
+        Button photo = (Button) rootView.findViewById(R.id.camera_button);
+        forward.setOnClickListener(this);
+        photo.setOnClickListener(this);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.forward_button:
+                break;
+            case R.id.camera_button:
+                dispatchTakePictureIntent();
+                break;
+        }
+    }
+
+}
