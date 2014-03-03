e032811fe05f25422e5fbede9bac978a106b3aaa
Howard Chen <howardxchen@gmail.com>
2014-03-02 16:54:28 -0800

    Create parent in AmountFragment

--- ff-android/ff-android/src/main/java/com/blueprint/ffandroid/AmountFragment.java
+++ ff-android/ff-android/src/main/java/com/blueprint/ffandroid/AmountFragment.java
@@ -33,14 +33,14 @@ public class AmountFragment extends Fragment implements View.OnClickListener {
 
     private OnFragmentInteractionListener mListener;
 
-
+    /** The parent activity of this fragment. */
+    private DonateActivity parent;
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment AmountFragment.
      */
     // TODO: Rename and change types and number of parameters
@@ -61,6 +61,7 @@ public class AmountFragment extends Fragment implements View.OnClickListener {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
         }
+        parent = (DonateActivity) getActivity();
     }
 
     @Override
