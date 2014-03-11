c31ed5d5f337000dfc5eb24ccea7a82ae1273156
varunrau <varunrau@gmail.com>
2014-03-10 17:59:24 -0700

    removed unnecessary cruft

--- ff-android/ff-android/src/main/java/com/blueprint/ffandroid/PhotoFragment.java
+++ ff-android/ff-android/src/main/java/com/blueprint/ffandroid/PhotoFragment.java
@@ -65,31 +65,6 @@ public class PhotoFragment extends Fragment implements View.OnClickListener {
         return rootView;
     }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
-
     public void setupClickListeners(View rootView) {
         Button forward = (Button) rootView.findViewById(R.id.forward_button);
         Button photo = (Button) rootView.findViewById(R.id.camera_button);
