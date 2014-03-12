6ca5bfcc58247170bd8c3989b9814176d52c199c
Howard Chen <howardxchen@gmail.com>
2014-03-11 21:45:59 -0700

    Add fb imports

--- ff-android/ff-android/src/main/java/com/blueprint/ffandroid/LoginFragment.java
+++ ff-android/ff-android/src/main/java/com/blueprint/ffandroid/LoginFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import com.facebook.*;
+import com.facebook.model.*;
 
 
 /**
