4daca3bd84cbe5c045a2853090b0e328f39fe7de
William Tang <williamctang93@gmail.com>
2014-03-20 18:33:05 -0700

    added ECSlidingViewController pod

--- ff-iOS/ff-iphone-app/Pods/Pods-environment.h
+++ ff-iOS/ff-iphone-app/Pods/Pods-environment.h
@@ -12,3 +12,9 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking 3
 #define COCOAPODS_VERSION_PATCH_AFNetworking 1
 
+// ECSlidingViewController
+#define COCOAPODS_POD_AVAILABLE_ECSlidingViewController
+#define COCOAPODS_VERSION_MAJOR_ECSlidingViewController 2
+#define COCOAPODS_VERSION_MINOR_ECSlidingViewController 0
+#define COCOAPODS_VERSION_PATCH_ECSlidingViewController 1
+
