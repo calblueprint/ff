1b175ef488716380bd5cf27ea9b8e835824c80db
varunrau <varunrau@gmail.com>
2014-03-28 20:19:55 -0400

    added form fragment

---
+++ ff-android/ff-android/src/main/java/com/blueprint/ffandroid/FormFragment.java
@@ -0,0 +1,34 @@
+package com.blueprint.ffandroid;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FormFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ *
+ */
+public class FormFragment extends Fragment {
+
+
+    public FormFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_form, container, false);
+    }
+
+}
