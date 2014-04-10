d4f967b96e6852997b9de19fe091403ff4f037de
Alton Zheng-Xie <altonzheng@berkeley.edu>
2014-04-09 19:25:20 -0700

    Code cleanup for PostDonationController.

--- ff-iOS/ff-iphone-app/FFiPhoneApp/Modules/PostDonation/POSDPostDonationViewController.m
+++ ff-iOS/ff-iphone-app/FFiPhoneApp/Modules/PostDonation/POSDPostDonationViewController.m
@@ -26,17 +26,6 @@ static NSString * const kDonationDescriptionPlaceholder = @"Add A Description Or
 
 @property (weak, nonatomic) AppDelegate *appDelegate;
 
-@property (strong, nonatomic) IBOutlet UITextField *textFieldDonationTitle;
-@property (strong, nonatomic) IBOutlet UITextView *textViewDonationDescription;
-@property (strong, nonatomic) IBOutlet UIView *viewTitleAndDescriptionBackground;
-@property (strong, nonatomic) IBOutlet UIButton *buttonPicture;
-@property (strong, nonatomic) IBOutlet UIButton *buttonWhere;
-@property (strong, nonatomic) IBOutlet UIButton *buttonHowMuch;
-@property (strong, nonatomic) IBOutlet UIButton *buttonWhen;
-@property (strong, nonatomic) IBOutlet UIButton *buttonPostDonation;
-@property (strong, nonatomic) IBOutlet UIView *viewButtonsBackground;
-@property (strong, nonatomic) IBOutlet UIButton *buttonCross;
-
 @property (strong, nonatomic) FFDataDonation *donation;
 @property (strong, nonatomic) UIImage *imageMealPhoto;
 @property (strong, nonatomic) UIImagePickerController *imagePicker;
@@ -49,23 +38,20 @@ static NSString * const kDonationDescriptionPlaceholder = @"Add A Description Or
 @property (strong, nonatomic) NSMutableDictionary *buttonImageViewCollection;
 @property (nonatomic) BOOL isPostDonationInProcess;
 
-- (IBAction)buttonPicture_onTouchUpInside:(id)sender;
-- (IBAction)buttonCross_onTouchUpInside:(id)sender;
-- (IBAction)donationTitle_onEditingChanged:(id)sender;
-
 // NEW STUFF
 - (IBAction)selectAddress:(id)sender;
 - (IBAction)selectPickupBy:(id)sender;
-- (IBAction)selectWeight:(id)sender;
-- (IBAction)selectKind:(id)sender;
 - (IBAction)buttonDone:(id)sender;
 
+
 @property (weak, nonatomic) IBOutlet UIButton *selectPickupByButton;
+
 @property (weak, nonatomic) IBOutlet UIButton *selectKindButton;
-@property (weak, nonatomic) IBOutlet UIButton *selectWeightButton;
+@property (weak, nonatomic) IBOutlet UITextField *selectKindField;
+
 @property (weak, nonatomic) IBOutlet UIButton *selectAddressButton;
 
-@property (weak, nonatomic) IBOutlet UITextField *selectKindField;
+@property (weak, nonatomic) IBOutlet UIButton *selectWeightButton;
 @property (weak, nonatomic) IBOutlet UITextField *selectDonationAmountField;
 
 
@@ -96,22 +82,16 @@ static NSString * const kDonationDescriptionPlaceholder = @"Add A Description Or
     [self.donation setLocation:[FFDataLocation new]];
     // Meta data for configuring buttons' appearance
   
-		[self.selectPickupByButton setTag:0];
-		[self.selectAddressButton setTag:1];
-		
-		// Set placehold for description field
-    [self setPlaceholderWithTextView:self.textViewDonationDescription text:kDonationDescriptionPlaceholder];
-    // Hide description field's cross button
-    [self.buttonCross setHidden:YES];
+		[self.selectKindButton setTag:0];
+		[self.selectWeightButton setTag:1];
+		[self.selectPickupByButton setTag:2];
+		[self.selectAddressButton setTag:3];
   
 	
 		self.selectKindField.delegate = self;
 		self.selectDonationAmountField.delegate = self;
 		self.selectDonationAmountField.keyboardType = UIKeyboardTypeDecimalPad;
     // Register to receive notification for prefilling the post donation form
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(didReceiveFFPostDonationPrefillPostDonationFormNotification:)
-                                                 name:@"FFPostDonationPrefillPostDonationFormNotification" object:nil];
 	
 }
 
@@ -145,76 +125,12 @@ static NSString * const kDonationDescriptionPlaceholder = @"Add A Description Or
           descriptionFontSize:13.0];
 //			self.selectAddressButton.titleLabel.text = self.donation.location.streetAddressOne;
     }
-//    // How Much?
-//    if (self.donation.totalLBS)
-//    {
-//        NSString *vehicleTypeDescription = nil;
-//        switch (self.donation.vehicleType) {
-//            case 0:
-//                vehicleTypeDescription = @"can be carried";
-//                break;
-//            case 1:
-//                vehicleTypeDescription = @"will fit in small vehicle";
-//                break;
-//            case 2:
-//                vehicleTypeDescription = @"will fit in medium vehicle";
-//                break;
-//            case 3:
-//                vehicleTypeDescription = @"will fit in large vehicle";
-//                break;
-//            default:
-//                vehicleTypeDescription = @"";
-//                
-//                DebugLog(@"Invalid vehicle type.");
-//                break;
-//        }
-//        [self setTextOnButton:self.buttonHowMuch
-//                        title:[NSString stringWithFormat:@"%u pounds", self.donation.totalLBS]
-//                titleFontSize:18.0
-//                  description:[NSString stringWithFormat:@"that %@", vehicleTypeDescription]
-//          descriptionFontSize:13.0];
-//    }
-//    // When?
-//    if (self.donation.availableStart && self.donation.availableEnd)
-//    {
-//        NSString *buttonTitle = nil;
-//        
-//        if ([self.donation.availableStart ff_isToday]) {
-//            buttonTitle = @"Today";
-//        }
-//        else if ([self.donation.availableStart ff_isTomorrow]) {
-//            buttonTitle = @"Tomorrow";
-//        }
-//        else {
-//            buttonTitle = [self.donation.availableStart ff_stringWithFormat:@"EEE, LLL d"];
-//        }
-//
-//        NSString *buttonDescription = [NSString stringWithFormat:@"from %@ \nto %@",
-//                             [self.donation.availableStart ff_stringWithFormat:@"hh:mm a"],
-//                             [self.donation.availableEnd ff_stringWithFormat:@"hh:mm a"]];
-//
-//        [self setTextOnButton:self.buttonWhen
-//                        title:buttonTitle
-//                titleFontSize:20.0
-//                  description:buttonDescription
-//          descriptionFontSize:13.0];
-//    }
-//    
-//    [self configurePostDonationButtonStatus];
+	[self configurePostDonationButtonStatus];
 }
 
 - (void)configurePostDonationButtonStatus
 {
-    if (self.donation.location.streetAddressOne
-        && self.donation.totalLBS
-        && self.donation.availableStart && self.donation.availableEnd
-        && self.donation.donationTitle.length)
-    {
-        [self.buttonPostDonation setEnabled:YES];
-    }
-    else {
-        [self.buttonPostDonation setEnabled:NO];
-    }
+
 }
 
 // Add attributed text to target button
@@ -295,138 +211,6 @@ static NSString * const kDonationDescriptionPlaceholder = @"Add A Description Or
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)didReceiveFFPostDonationPrefillPostDonationFormNotification:(id)sender
-{
-    // Go back to main screen
-    [self.navigationController popToRootViewControllerAnimated:YES];
-
-    // Reset buttons appearance
-    [self configureAppearance];
-
-    FFDataDonation *donation = [[sender userInfo] ff_objectForKey:@"donation"];
-    
-    // Meal photo
-    UIImage *mealPhoto = [[FFImageStore sharedStore] imageForKey:donation.mealPhoto.imageURL];
-    [self setImageMealPhoto:mealPhoto];
-    [self setImageOnButton:self.buttonPicture image:mealPhoto];
-    // Location
-    [self.donation setLocation:donation.location];
-    // Total pounds
-    [self.donation setTotalLBS:donation.totalLBS];
-    // Title
-    [self.donation setDonationTitle:donation.donationTitle];
-    [self.textFieldDonationTitle setText:donation.donationTitle];
-    // Description
-    [self.donation setDonationDescription:donation.donationDescription];
-    if ([donation.donationDescription length] == 0) {
-        [self setPlaceholderWithTextView:self.textViewDonationDescription text:kDonationDescriptionPlaceholder];
-    }
-    else {
-        [self clearPlaceholderWithTextView:self.textViewDonationDescription];
-        [self.textViewDonationDescription setText:donation.donationDescription];
-    }
-    [self.donation setVehicleType:donation.vehicleType];
-
-    [self viewWillAppear:NO];
-}
-
-- (void)animateView:(UIView *)view up:(BOOL)up
-{
-    [FFUI scrollUpView:self.view withDirectionUp:up distance:165];
-}
-
-- (void)setImageOnButton:(UIButton *)button image:(UIImage *)image
-{    
-    if (!self.buttonImageViewCollection) {
-        self.buttonImageViewCollection = [NSMutableDictionary dictionary];
-    }
-    
-    NSString *buttonTag = [NSString stringWithFormat:@"%d", button.tag];
-    UIImageView *imageView = [self.buttonImageViewCollection objectForKey:buttonTag];
-    
-    if (!imageView) {
-        imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0,
-                                                      CGRectGetWidth(button.bounds),
-                                                      CGRectGetHeight(button.bounds))];
-        imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-        
-        [imageView setContentMode:UIViewContentModeScaleAspectFit];
-        [self.buttonImageViewCollection setObject:imageView forKey:buttonTag];
-        
-        // Put the image view on top of target button
-        [button addSubview:imageView];
-    }
-
-    [imageView setImage:image];
-
-    if (image) {
-        // Change target button's images to reflect its new state
-        [button setImage:[button imageForState:UIControlStateSelected] forState:UIControlStateHighlighted];
-        [button setImage:[button imageForState:UIControlStateSelected] forState:UIControlStateNormal];
-    }
-}
-
-- (void)setPlaceholderWithTextView:(UITextView *)textView text:(NSString *)text
-{
-    [textView setText:text];
-    [textView setTag:1];
-    [textView setTextColor:[UIColor lightGrayColor]];
-}
-
-- (void)clearPlaceholderWithTextView:(UITextView *)textView
-{
-    [textView setText:@""];
-    [textView setTag:0];
-    [textView setTextColor:[UIColor darkGrayColor]];
-}
-
-- (void)clearDonationDescriptionText
-{
-    [self.textViewDonationDescription setText:@""];
-}
-
-- (void)configureCrossButtonStatus
-{
-    if ([self.textViewDonationDescription.text length] > 0) {
-        [self.buttonCross setHidden:NO];
-    }
-    else {
-        [self.buttonCross setHidden:YES];
-    }
-}
-
-- (IBAction)buttonPicture_onTouchUpInside:(id)sender
-{
-    [self presentPictureSourceOptions];
-}
-
-- (IBAction)buttonCross_onTouchUpInside:(id)sender
-{
-    [self clearDonationDescriptionText];
-    [self.textViewDonationDescription.delegate textViewDidChange:self.textViewDonationDescription];
-}
-
-- (IBAction)buttonPostDonation_onTouchUpInside:(id)sender
-{
-    [self.view endEditing:YES];
-    [self.buttonPostDonation setEnabled:NO];
-    [FFUI showLoadingViewOnView:self.view visible:YES];
-    [self setIsPostDonationInProcess:YES];
-    
-    [self postDonationWithSuccess:nil failure:^{
-        [self.buttonPostDonation setEnabled:YES];
-        [self setIsPostDonationInProcess:NO];
-        [FFUI showLoadingViewOnView:self.view visible:NO];
-    }];
-
-}
-
-- (IBAction)donationTitle_onEditingChanged:(id)sender
-{
-    [self.donation setDonationTitle:self.textFieldDonationTitle.text];
-    [self configurePostDonationButtonStatus];
 }
 
 //
@@ -541,85 +325,14 @@ static NSString * const kDonationDescriptionPlaceholder = @"Add A Description Or
     }
 }
 
-// UIActionSheet delegate method
-- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
-{
-    if (buttonIndex == 0) {
-       self.imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
-    } else if (buttonIndex == 1) {
-        self.imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-    } else {
-        return;
-    }
-    
-    [self presentViewController:self.imagePicker animated:YES completion:nil];
-}
-
 // UIImagePickerController delegate method
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
     [self setImageMealPhoto:[info objectForKey:UIImagePickerControllerEditedImage]];
     [self dismissViewControllerAnimated:YES completion:nil];
 
-    [self setImageOnButton:self.buttonPicture image:self.imageMealPhoto];
 }
 //---------------------------------------------
-
-- (BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender
-{
-    if ([identifier isEqualToString:@"presentChooseLocation"])
-    {
-        //
-        // Present Choose Location view controller
-        //
-        if (!self.chooseLocationViewController) {
-            self.chooseLocationViewController = [self.moduleController.storyboard instantiateViewControllerWithIdentifier:@"POSDChooseLocationViewController"];
-            
-            // Pass location to Choose Location view controller
-            [self.chooseLocationViewController setLocation:self.donation.location];
-        }
-        
-        [self.navigationController pushViewController:(id)self.chooseLocationViewController animated:YES];
-
-        return NO;
-    }
-    
-    if ([identifier isEqualToString:@"presentChooseAmount"])
-    {
-        //
-        // Present Choose Amount view controller
-        //
-        if (!self.chooseAmountViewController) {
-            self.chooseAmountViewController = [self.moduleController.storyboard instantiateViewControllerWithIdentifier:@"POSDChooseAmountViewController"];
-            
-            // Pass donation to Choose Amount view controller
-            [self.chooseAmountViewController setDonation:self.donation];
-        }
-
-        [self.navigationController pushViewController:(id)self.chooseAmountViewController animated:YES];
-
-        return NO;
-    }
-    
-    if ([identifier isEqualToString:@"presentChooseTime"])
-    {
-        //
-        // Present Choose Time view controller
-        //
-        if (!self.chooseTimeViewController) {
-            self.chooseTimeViewController = [self.moduleController.storyboard instantiateViewControllerWithIdentifier:@"POSDChooseTimeViewController"];
-            
-            // Pass donation to Choose Time view controller
-            [self.chooseTimeViewController setDonation:self.donation];
-        }
-        
-        [self.navigationController pushViewController:(id)self.chooseTimeViewController animated:YES];
-        
-        return NO;
-    }
-    
-    return YES;
-}
 
 #pragma mark - UITextField delegate
 
@@ -635,79 +348,33 @@ static NSString * const kDonationDescriptionPlaceholder = @"Add A Description Or
 //    [self animateView:textField up: YES];
   
     // Disable user interaction on all buttons
-    [self.viewButtonsBackground setUserInteractionEnabled:NO];
+	if (textField == self.selectKindField) {
+    textField.text = self.selectKindButton.titleLabel.text;
+	} else if (textField == self.selectDonationAmountField) {
+		textField.text = self.selectWeightButton.titleLabel.text;
+	}
 }
 
 - (void)textFieldDidEndEditing:(UITextField *)textField
 {
     // Enable user interaction on all buttons
-    [self.viewButtonsBackground setUserInteractionEnabled:YES];
 
     if (textField == self.selectKindField) {
-//        [self.donation setDonationTitle:textField.text];
-//        [self configurePostDonationButtonStatus];
-    }
+			[self setTextOnButton:self.selectKindButton
+											title:textField.text
+							titleFontSize:18.0
+								description:@""
+				descriptionFontSize:13.0];
+    } else if (textField == self.selectDonationAmountField) {
+			[self setTextOnButton:self.selectWeightButton
+											title:textField.text
+							titleFontSize:18.0
+								description:@"pounds"
+				descriptionFontSize:13.0];
+		}
+	textField.text = @"";
 	[textField resignFirstResponder];
 }
-
-//#pragma mark - UITextView delegate
-//
-//- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
-//{
-//    if ([text isEqualToString:@"\n"]) {
-//        [textView resignFirstResponder];
-//    }
-//    return YES;
-//}
-//
-//- (void)textViewDidChange:(UITextView *)textView
-//{
-//    if (textView == self.textViewDonationDescription)
-//    {
-//        [self configureCrossButtonStatus];
-//    }
-//}
-//
-//- (void)textViewDidBeginEditing:(UITextView *)textView
-//{
-//    [self animateView:textView up:YES];
-//    
-//    // Disable user interaction on all buttons
-//    [self.viewButtonsBackground setUserInteractionEnabled:NO];
-//    
-//    if (textView == self.textViewDonationDescription)
-//    {
-//        // textViewDonationDescription is containing placeholder text
-//        if (textView.tag) {
-//            [self clearPlaceholderWithTextView:textView];
-//        }
-//        else {
-//            [self configureCrossButtonStatus];
-//        }
-//    }
-//}
-//
-//- (void)textViewDidEndEditing:(UITextView *)textView
-//{
-//    [self animateView:textView up:NO];
-//    
-//    // Enable user interaction on all buttons
-//    [self.viewButtonsBackground setUserInteractionEnabled:YES];
-//    
-//    if (textView == self.textViewDonationDescription)
-//    {
-//        if ([textView.text length] == 0) {
-//            [self setPlaceholderWithTextView:textView text:kDonationDescriptionPlaceholder];
-//            [self.donation setDonationDescription:@""];
-//        }
-//        else {
-//            [self.donation setDonationDescription:textView.text];
-//        }
-//        
-//        // Hide clear button
-//        [self.buttonCross setHidden:YES];
-//    }
-//}
 
 
 - (IBAction)selectAddress:(id)sender {
