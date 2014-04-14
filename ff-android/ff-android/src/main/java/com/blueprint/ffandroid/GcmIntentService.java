e24d8b446f446b68b603ad2aef7c8fe1edf53a52
Howard Chen <howardxchen@gmail.com>
2014-04-13 18:29:07 -0700

    Use notification builder

--- ff-android/ff-android/src/main/java/com/blueprint/ffandroid/GcmIntentService.java
+++ ff-android/ff-android/src/main/java/com/blueprint/ffandroid/GcmIntentService.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Looper;
 import android.os.SystemClock;
@@ -66,16 +67,6 @@ public class GcmIntentService extends IntentService {
                 Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
                 // Post notification of received message.
                 sendNotification("Received: " + extras.toString());
-                Log.i(TAG, "Received: " + extras.toString());
-                Handler h = new Handler(Looper.getMainLooper());
-                final Context c = this.getApplicationContext();
-                h.post(new Runnable() {
-
-                    public void run() {
-                        // TODO Auto-generated method stub
-                        Toast.makeText(c, extras.toString(), Toast.LENGTH_SHORT).show();
-                    }
-                });
             }
         }
         // Release the wake lock provided by the WakefulBroadcastReceiver.
@@ -94,12 +85,10 @@ public class GcmIntentService extends IntentService {
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
-                        .setContentTitle("GCM Notification")
-                        .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText(msg))
-                        .setContentText(msg);
-
+                        .setContentTitle("New Donation " + msg)
+                        .setContentText(msg)
+                        .setSmallIcon(R.drawable.ic_launcher);
         mBuilder.setContentIntent(contentIntent);
-        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+        mNotificationManager.notify(0, mBuilder.build());
     }
 }
