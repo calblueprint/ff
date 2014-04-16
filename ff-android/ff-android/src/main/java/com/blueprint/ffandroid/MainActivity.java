ccef65ba95118059f32f7ea2608a031cbddb093e
Tony Wu <tony.wu@berkeley.edu>
2014-04-15 18:17:12 -0700

    changed FAQ to Frequently Asked Questions

--- ff-android/ff-android/src/main/java/com/blueprint/ffandroid/MainActivity.java
+++ ff-android/ff-android/src/main/java/com/blueprint/ffandroid/MainActivity.java
@@ -137,7 +137,7 @@ public class MainActivity extends ActionBarActivity
                 mTitle = "Account Details";
                 break;
             case 3:
-                mTitle = "FAQ";
+                mTitle = "Frequently Asked Questions";
                 break;
             case 4:
                 mTitle = "Logout";
