163db0256365f84d66d11094055c97e365cc840b
Nishant Desai <nishantdesai@berkeley.edu>
2014-04-17 02:24:01 -0700

    Git said LoginActivity was changed. I don't buy it

--- ff-android/ff-android/src/main/java/com/blueprint/ffandroid/LoginActivity.java
+++ ff-android/ff-android/src/main/java/com/blueprint/ffandroid/LoginActivity.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 import com.android.volley.NetworkError;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.Request;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
