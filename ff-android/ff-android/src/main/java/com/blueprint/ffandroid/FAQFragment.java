d0ab1a7299bddc2b31654ab2ea3072f0d6abac7d
Howard Chen <howardxchen@gmail.com>
2014-04-19 20:54:07 -0700

    Add font to FAQ

--- ff-android/ff-android/src/main/java/com/blueprint/ffandroid/FAQFragment.java
+++ ff-android/ff-android/src/main/java/com/blueprint/ffandroid/FAQFragment.java
@@ -1,6 +1,7 @@
 package com.blueprint.ffandroid;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,9 +10,21 @@ import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.support.v4.app.Fragment;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class FAQFragment extends Fragment {
+
+    /** The button that links to the feeding forward. */
+    Button link;
+    /** The title of the FAQ Fragment.*/
+    TextView faq;
+    /** The About Feeding Forward Header. */
+    TextView faqHeader;
+    /** The short description of Feeding Forward. */
+    TextView faqDescription;
 
     public static FAQFragment newInstance() {
         FAQFragment fragment = new FAQFragment();
@@ -43,6 +56,22 @@ public class FAQFragment extends Fragment {
             }
 
         });
+        faq = (TextView) rootView.findViewById(R.id.faq);
+        faqHeader = (TextView) rootView.findViewById(R.id.about_feeding_forward);
+        faqDescription = (TextView) rootView.findViewById(R.id.about_description);
+        setFonts();
         return rootView;
+    }
+
+    /**
+     * Sets the fonts of the Buttons and TextViews in this fragment
+     */
+    private void setFonts(){
+        MainActivity parent = (MainActivity)this.getActivity();
+        Typeface tf = parent.myTypeface;
+        faq.setTypeface(tf);
+        faqHeader.setTypeface(tf);
+        faqDescription.setTypeface(tf);
+
     }
 }
