45a4bff03383b1e0368051e364c3205c6f422639
varunrau <varunrau@gmail.com>
2014-05-07 11:38:46 -0700

    removed old imports

--- ff-android/ff-android/src/main/java/com/blueprint/ffandroid/MainActivity.java
+++ ff-android/ff-android/src/main/java/com/blueprint/ffandroid/MainActivity.java
@@ -11,25 +11,17 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Spannable;
-import android.text.SpannableString;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.MenuItem;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
-
-import junit.runner.Version;
 
 import java.io.IOException;
 import java.util.ArrayList;
