58844d85acd6fe2deac2c39093df89092045bff2
Howard Chen <howardxchen@gmail.com>
2014-05-07 02:46:29 -0700

    Remove extra stuff

--- ff-android/ff-android/src/main/java/com/blueprint/ffandroid/DonationDetailFragment.java
+++ ff-android/ff-android/src/main/java/com/blueprint/ffandroid/DonationDetailFragment.java
@@ -1,33 +1,17 @@
 package com.blueprint.ffandroid;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.content.res.ColorStateList;
-import android.content.res.Resources;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.graphics.Typeface;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.content.Intent;
-import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 
